4b6ea2ceb1f7f5f227f1b096f888562db91b3a61
zenith391 <pawalamde@gmail.com>
2020-02-01 11:00:30 +0100

    Use builders

--- g-gen.v
+++ g-gen.v
@@ -1,33 +1,33 @@
 import vgobject.gi
-/*
-fn gen_cdefs(namespace string, repo &gi.Repository) {
-	str := ""
-	info_count := repo.get_n_infos(namespace)
+import os
+import strings
 
+fn gen_cdefs(namespace string, repo &gi.Repository) string {
+	mut builder := strings.new_builder(1024)
 	for i := 0; i < info_count; i++ {
 		bi := repo.get_info(namespace, i)
 		name := bi.get_name()
 		typ := bi.get_type()
 		type_str := typ.str()
-		println(name)
 		if (typ == gi.INFO_TYPE_OBJECT) {
-			object := bi.to_object_info()
+			oi := bi.to_object_info()
+			builder.write(object_cdefs(oi))
 		}
 	}
+	return builder.str()
 }
-*/
 
 fn object_cdefs(info &gi.ObjectInfo) string {
-	mut str := ""
-	str = str + "/// " + info.get_type_name() + " ///\n"
+	mut builder := strings.new_builder(1024)
+	builder.writeln("/// " + info.get_type_name() + " ///")
 	method_count := info.get_n_methods()
 	for j := 0; j < method_count; j++ {
 		fi := info.get_method(j)
 		fbi := &gi.BaseInfo(fi)
 		fdef := "fn " + info.get_type_init() + "_" + fbi.get_name() + "(...)"
-		str = str + fdef + "\n"
+		builder.writeln(fdef)
 	}
-	return str
+	return builder.str()
 }
 
 fn main() {
@@ -37,19 +37,8 @@ fn main() {
 		eprintln("could not find '" + namespace + "' typelib: " + err)
 		exit(1)
 	}
-
+	os.mkdir("out")
 	info_count := repo.get_n_infos(namespace)
 
-	for i := 0; i < info_count; i++ {
-		bi := repo.get_info(namespace, i)
-		name := bi.get_name()
-		typ := bi.get_type()
-		type_str := typ.str()
-		if (typ == gi.INFO_TYPE_OBJECT) {
-			oi := bi.to_object_info()
-			println(object_cdefs(oi))
-		}
-	}
-
-	//println(gen_cdefs(namespace, repo))
+	println(gen_cdefs(namespace, repo))
 }
