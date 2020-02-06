013cd92689c4ad4cd633133fb4d35c122771c7d1
Don Alfons Nisnoni <donnisnoni.tid3@gmail.com>
2020-02-07 04:03:35 +0800

    Cleanup: - Load all Gtk dependencies - Handling mkdir - Fix get real gtk function name - Using new vgobject casting helper function

--- g-gen.v
+++ g-gen.v
@@ -1,16 +1,17 @@
+module main
+
 import vgobject.gi
 import os
 import strings
 
 fn gen_cdefs(namespace string, repo &gi.Repository) string {
 	mut builder := strings.new_builder(1024)
-	for i := 0; i < info_count; i++ {
+	n_info := repo.get_n_infos(namespace)
+	for i := 0; i < n_info; i++ {
 		bi := repo.get_info(namespace, i)
-		name := bi.get_name()
 		typ := bi.get_type()
-		type_str := typ.str()
 		if (typ == gi.INFO_TYPE_OBJECT) {
-			oi := bi.to_object_info()
+			oi := gi.to_object_info(bi)
 			builder.write(object_cdefs(oi))
 		}
 	}
@@ -20,11 +21,12 @@ fn gen_cdefs(namespace string, repo &gi.Repository) string {
 fn object_cdefs(info &gi.ObjectInfo) string {
 	mut builder := strings.new_builder(1024)
 	builder.writeln("/// " + info.get_type_name() + " ///")
-	method_count := info.get_n_methods()
-	for j := 0; j < method_count; j++ {
+	n_methods := info.get_n_methods()
+	for j := 0; j < n_methods; j++ {
 		fi := info.get_method(j)
-		fbi := &gi.BaseInfo(fi)
-		fdef := "fn " + info.get_type_init() + "_" + fbi.get_name() + "(...)"
+		mut fdef := "fn " + fi.get_symbol() + "("
+		// TODO: Get property info of this method
+		fdef += ")"
 		builder.writeln(fdef)
 	}
 	return builder.str()
@@ -33,12 +35,15 @@ fn object_cdefs(info &gi.ObjectInfo) string {
 fn main() {
 	namespace := "Gtk"
 	repo := gi.get_default_repository()
-	repo.require(namespace, "") or {
-		eprintln("could not find '" + namespace + "' typelib: " + err)
-		exit(1)
-	}
-	os.mkdir("out")
-	info_count := repo.get_n_infos(namespace)
+	repo.require(namespace, "") or { panic(err) }
+	repo.require("GLib", "") or { panic(err) }
+	repo.require("Gdk", "") or { panic(err) }
+	repo.require("Pango", "") or { panic(err) }
 
-	println(gen_cdefs(namespace, repo))
+	if !os.exists('out') {
+		os.mkdir("out") or { panic(err) }
+	}
+
+	cdefs := gen_cdefs(namespace, repo)
+	println(cdefs)
 }
